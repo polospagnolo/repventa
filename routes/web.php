1ba4577b1366d3d37ff115287f27856f75d033c6
José Antonio Rojas Rodríguez <joseantoniorojasrodriguez11@gmail.com>
2019-02-12 14:28:54 +0100

    add auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
     dd($user->hasRole(['super-admin']));
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
