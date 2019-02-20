7eb048939cc8961508d00328a0eb1cb7a738ec56
Polo Spagnolo <joseantoniorojas@spagnolo.com.es>
2019-02-20 17:08:35 +0100

    add name to app

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@import')->name('home');
 
